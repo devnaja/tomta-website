2231c4755c30d80877a2b1a672baf8ea12c1a0e6
ADMINPC\Tomta <naja.nadhirah@tomtatechnology.com>
2023-12-19 10:28:39 +0800

    change font size for banner section

--- src/components/BannerBg.js
+++ src/components/BannerBg.js
@@ -8,7 +8,7 @@ function BannerBg({ image, text }) {
     >
       <div className=" bg-slate-900 opacity-50 h-[40vh] lg:h-[50vh] xl:h-screen  w-full"></div>
 
-      <div className="absolute text-7xl uppercase font-bold text-white h-[30rem] w-full flex justify-center items-center leading-relaxed tracking-widest">
+      <div className="absolute text-2xl md:text-4xl lg:text-7xl uppercase font-bold text-white h-[30rem] w-full flex justify-center items-center leading-relaxed tracking-widest">
         {text}
       </div>
     </div>
